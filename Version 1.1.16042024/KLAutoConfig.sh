bf451a18fed95b58a0cfdf4c2867cd6660d265a6
NAVIDGOLPA <navid.golpa.dev.official@gmail.com>
2024-04-16 17:45:41 +0400

    docs: Header description was added to the KLAutoConfig.sh file.

---
+++ Version 1.1.16042024/KLAutoConfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This Shell Script installs all the necessary tools for network security and penetration testing on Kali Linux.
+# THIS SYSTEM IS COMPLETELY FREE AND OPEN-SOURCE AND IS NOT AFFILIATED WITH ANY GOVERNMENTAL OR PRIVATE ORGANIZATION.
+
+# (C) 2024, by: NAVID GOLPAYEGANI
+# Savojbolagh, Hashtgerd / Mahestan - Iran
